861d130c9e13c8e71108ec38115203ae8f67d755
Nereida Aleman <109105366+Naleman9@users.noreply.github.com>
2023-02-11 16:42:19 -0300

    Update PersonaController.java

--- src/main/java/com/portfolio/nab/Controller/PersonaController.java
+++ src/main/java/com/portfolio/nab/Controller/PersonaController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"https://frontendnab.web.app"})
+@CrossOrigin(origins = "https://frontendnab.web.app")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
